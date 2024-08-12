acb29ced10a72e385a8ad851fcb5e24c7f8ed2fb
22051339 <himanshusaini0803@gmail.com>
2024-08-12 22:12:23 +0530

    test

--- Backend/app.js
+++ Backend/app.js
@@ -3,11 +3,11 @@ import cors from "cors";
 
 const app = express();
 
-app.use(
-    cors({
-        origin: "http://localhost:5173",
-    })
-);
+// app.use(
+//     cors({
+//         origin: "http://localhost:5173",
+//     })
+// );
 
 app.use(express.json({ limit: "16kb" }));
 app.use(express.urlencoded({ extended: true, limit: "16kb" }));
